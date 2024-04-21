06abc773249ea0f185acec88193ba78caf9fdea1
Jayasuryan <127474567+Jayasuryan99@users.noreply.github.com>
2024-04-21 20:20:23 +0530

    Update Consignee.jsx

--- src/allBills/Consignee.jsx
+++ src/allBills/Consignee.jsx
@@ -79,9 +79,9 @@ function Consignee(props) {
             <p>
               <strong> Commission Agent </strong>
             </p>
-            <p>
-              B.O : Shop No.1, H.No.:5-29/2, Near Railway Bridge, NH-44, THONDUPALLY(V), SHAMSHABAD(M), Ranga Reddy District- 509 325. HYDRABAD (Telangana.)
-              H.O :  No.4/181,Plot No. 8,1st Floor Sri Ram Tower,Thuraiyur Road,Anna Nagar,NAMAKKAL-637 002              </p>
+            <p> H.O :  No.4/181,Plot No. 8,1st Floor Sri Ram Tower,Thuraiyur Road,Anna Nagar,NAMAKKAL-637 002 
+                B.O : Shop No.1, H.No.:5-29/2, Near Railway Bridge, NH-44, THONDUPALLY(V), SHAMSHABAD(M), Ranga Reddy District- 509 325. HYDRABAD (Telangana.)
+                           </p>
           <p> Cell: 91525 25000, 91525 26000 E-Mail:hydjaibalaji@gmail.com</p>
           </div>
         </div>
