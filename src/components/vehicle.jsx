02a5641f15c94bf0c50b3e1ce610b6c963449a06
Jayasuryan <127474567+Jayasuryan99@users.noreply.github.com>
2024-04-21 20:07:26 +0530

    Update vehicle.jsx

--- src/components/vehicle.jsx
+++ src/components/vehicle.jsx
@@ -302,12 +302,12 @@ function Vehicle() {
         </div>
         <div className="w-[100vw] lg:w-[78vw] flex justify-end items-end gap-5 pr-3   md:pr:10 lg:pr-20">
           <div
-            className=" bg-[--primary-color] h-[30px] !text-[10px] lg:!text-[14px] px-3 lg:px-5 py-1 rounded-md cursor-pointer text-white font-bold  flex items-center justify-center "
+            className=" bg-[--secondary-color] h-[30px] !text-[10px] lg:!text-[14px] px-3 lg:px-5 py-1 rounded-md cursor-pointer text-white font-bold  flex items-center justify-center "
             onClick={() => {
               setOpen(true);
             }}
           >
-            <AddOutlinedIcon className="!text-[18px] lg:!text-[22px]"/> Create
+            <AddOutlinedIcon className=" bg-[--secondary-color] !text-[18px] lg:!text-[22px]"/> Create
           </div>
           <div>
             <Button
