c541a7d3467d19321c29a2645ddacf233155c1f2
Jayasuryan <127474567+Jayasuryan99@users.noreply.github.com>
2024-04-21 20:28:57 +0530

    Update VehicleBills.jsx

--- src/Bills/VehicleBills.jsx
+++ src/Bills/VehicleBills.jsx
@@ -90,7 +90,7 @@ function VehicleBills() {
             <strong> Commission Agent </strong>
           </p>
           <p> H.O :  No.4/181,Plot No. 8,1st Floor Sri Ram Tower,Thuraiyur Road,Anna Nagar,NAMAKKAL-637 002. </p>
-          <p> B.O : Shop No.1,H.No.:5-29/2,Near Railway Bridge,NH-44,Thoundupally(V),Shamshabad(M),Ranga Reddy District-509 325.HYDERABAD(TS)
+<p> B.O : Shop No.1,H.No.:5-29/2,Near Railway Bridge,NH-44,Thoundupally(V),Shamshabad(M),Ranga Reddy District- 509 325.HYDERABAD(TS)
               
 </p>           
             <p> Cell: 91525 25000, 91525 26000 E-Mail:hydjaibalaji@gmail.com </p>
